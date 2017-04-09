361baec4786c958b2b83705587d25876844a164a
Dwonczykj <jdwonczyk@gmail.com>
2017-04-09 01:13:48 +0100

    Init
    Need to fix magic array get in AuthService.js

---
+++ AuthService.js
@@ -0,0 +1,75 @@
+
+import {AsyncStorage} from 'react-native';
+import _ from 'lodash';
+var encoding = require('NativeModules').Encoding;
+
+const authKey = 'auth';
+const userKey = 'user';
+
+
+export const getAuthInfo = (cb) => {
+    AsyncStorage.multiGet([authKey, userKey], (err, val)=> {
+        if(err){
+            return cb(err);
+        }
+
+        if(!val){
+            return cb();
+        }
+
+        var zippedObj = _.zipObject([authKey, userKey],[val[0][1],val[1][1]]);
+
+        if(!zippedObj[authKey]){
+            return cb();
+        }
+
+        var authInfo = {
+            header: {
+                Authorization: 'Basic ' + zippedObj[authKey]
+            },
+            user: JSON.parse(zippedObj[userKey])
+        }
+
+        return cb(null, authInfo);
+    });
+};
+
+export const login = (creds, cb) => {
+    var authStr = creds.username + ':' + creds.password;
+    encoding.base64Encode(authStr, (encodedAuth)=>{
+        fetch('https://api.github.com/user',{
+            headers: {
+                'Authorization' : 'Basic ' + encodedAuth
+            }
+        })
+        .then((response)=> {
+            if(response.status >= 200 && response.status < 300){
+                return response;
+            }
+
+            throw {
+                badCredentials: response.status == 401,
+                unknownError: response.status != 401
+            }
+        })
+        .then((response)=> {
+            return response.json();
+        })
+        .then((results)=> {
+            debugger;
+            AsyncStorage.multiSet({
+                [authKey]: encodedAuth,
+                [userKey]: JSON.stringify(results)
+            }, (err)=> {
+                if(err){
+                    throw err;
+                }
+
+                return cb({success: true});
+            });
+        })
+        .catch((err)=> {
+            return cb(err);
+        });
+    });
+};
---
+++ index.ios.js
@@ -0,0 +1,100 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator
+} from 'react-native';
+
+import Login from './Login';
+import AppContainer from './AppContainer';
+import {getAuthInfo} from './AuthService';
+
+export default class BlankReactNativeProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkingAuth: true,
+      isLoggedIn: false
+    }
+    this.onLogin = this.onLogin.bind(this);
+  }
+
+  componentDidMount() {
+    getAuthInfo((err, authInfo) => {
+      this.setState({
+        checkingAuth: false,
+        isLoggedIn: authInfo != null
+      })
+    });
+  }
+
+  onLogin(){
+    this.setState({isLoggedIn: true});
+  }
+
+  render() {
+    // return (
+    //   <View style={styles.container}>
+    //     <Text style={styles.welcome}>
+    //       Welcome to React Native!
+    //     </Text>
+    //     <Text style={styles.instructions}>
+    //       To get started, edit index.ios.js
+    //     </Text>
+    //     <Text style={styles.instructions}>
+    //       Press Cmd+R to reload,{'\n'}
+    //       Cmd+D or shake for dev menu
+    //     </Text>
+    //   </View>
+    // );
+    if(this.state.checkingAuth){
+      return (
+        <View style={styles.container}>
+        <ActivityIndicator
+          animating={true}
+          size="large"
+          style={styles.loader} />
+        </View>
+      );
+    }
+
+    if(this.state.isLoggedIn){
+      return (
+        <AppContainer />
+      );
+    }else{
+      return (
+        <Login onLogin={this.onLogin} />
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('BlankReactNativeProject', () => BlankReactNativeProject);
